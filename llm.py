1ef5a90d4ca22b392cbe60b115e2e70acc016a80
dpruden13 <dpruden13@gmail.com>
2025-12-14 14:07:16 +0100

    added more Exception handling to call_llm()

--- llm.py
+++ llm.py
@@ -3,6 +3,13 @@ from ollama import chat, ChatResponse
 from openai import OpenAI
 
 
+MODEL_TYPES = [
+    basic := 'basic',
+    medium := 'medium',
+    best := 'best',
+    ]
+
+
 def call_deepseek(prompt: str) -> str:
     # https://huggingface.co/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
     pipe = pipeline("text-generation", model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
@@ -24,12 +31,21 @@ def call_openai(prompt: str) -> str:
     return response.output_text
 
 
-def call_llm(prompt: str, model_type: str = 'basic') -> str:
-    if model_type == 'basic':
-        return call_deepseek(prompt)
-    elif model_type == 'medium':
-        return call_gemma(prompt)
-    elif model_type == 'best':
-        return call_openai(prompt)
-    else:
-        raise Exception(f'Unrecognized model_type "{model_type}", should be "basic", "medium", or "best".')
+def call_llm(prompt: str, model_type: str = best, retry: bool = True) -> str:
+    try:
+        if model_type == basic:
+            return call_deepseek(prompt)
+        elif model_type == medium:
+            return call_gemma(prompt)
+        elif model_type == best:
+            return call_openai(prompt)
+        else:
+            raise Exception(f'Unrecognized model_type "{model_type}", should be one of {MODEL_TYPES}')
+    except Exception:
+        if retry:
+            model_type_index = MODEL_TYPES.index(model_type)
+            if model_type_index == 0:
+                raise
+            return call_llm(prompt=prompt, model_type=MODEL_TYPES[model_type_index - 1], retry=retry)
+        else:
+            raise
