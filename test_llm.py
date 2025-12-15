f54990a6a82a069133d5b94784dd23d2c6e48502
dpruden13 <dpruden13@gmail.com>
2025-12-15 15:38:56 +0100

    minor improvements

--- test_llm.py
+++ test_llm.py
@@ -2,11 +2,11 @@ from llm import call_llm, medium, basic
 
 
 def test_best_model_does_not_crash():
-    call_llm(prompt="What are some adventure movies in this dataset?")
+    call_llm(prompt="What are some adventure movies in this dataset?", retry=False)
 
 
 def test_medium_model_does_not_crash():
-    call_llm(prompt="Which movies were directed by James Cameron?", model_type=medium)
+    call_llm(prompt="Which movies were directed by James Cameron?", model_type=medium, retry=False)
 
 
 def test_basic_model_does_not_crash():
