cc884f301e10a2b112acfdff19f2a7edac2fde1c
dpruden13 <dpruden13@gmail.com>
2025-12-15 09:23:44 +0100

    improved parameterization

--- query.py
+++ query.py
@@ -1,32 +1,28 @@
 from json import loads
 
-from llm import call_llm, medium
+from llm import call_llm, best
 from database import query_database
-
-import pandas as pd
 
 
 def detect_intent(user_prompt: str) -> str:
     # Can result in JSONDecodeError
-    return loads(call_llm(
-        prompt=(
-            f'Which topic does the following user prompt relate to? {user_prompt} '
-            'Respond ONLY with a JSON object with a KEY and VALUE. '
-            'The KEY should be "genres" or "ratings" or "title" or "year" or "director" or "plot" or "other". '
-            'The VALUE should be the specific data for that KEY. '
-            'Example: {"genres": "adventure"} '
-            'Another example: {"year": "1997"} '
-            'Another example: {"cast": "Kate Winslet"} '
-            'Another example: {"director": "James Cameron"} '
-            'Another example: {"plot": "ship sinks"} '
-            'Another example: {"other": "highest budget movie"}'
-            ),
-        model_type=medium,
-        retry=False,  # the 'basic' model ignores formatting instructions
-        ).strip().removeprefix('```json').removesuffix('```').strip())  # remove unnecessary text
+    return loads(
+        call_llm(
+            prompt=(
+                f'Which topic does the following user prompt relate to? {user_prompt} '
+                'Respond ONLY with a JSON object with a KEY and VALUE. '
+                'The KEY should be "genres" or "ratings" or "title" or "year" or "director" or "plot" or "other". '
+                'The VALUE should be the specific data for that KEY. '
+                'Example: {"genres": "adventure"} '
+                'Another example: {"year": "1997"} '
+                'Another example: {"cast": "Kate Winslet"} '
+                'Another example: {"director": "James Cameron"} '
+                'Another example: {"plot": "ship sinks"} '
+                'Another example: {"other": "highest budget movie"}'
+                ).strip().removeprefix('```json').removesuffix('```').strip()))  # remove unnecessary text
 
 
-def answer_query(user_prompt: str) -> str:
+def answer_query(user_prompt: str, model_type: str = best, retry: bool = True) -> str:
     intent = detect_intent(user_prompt)
     topic, data = list(intent.items())[0]
     if topic == 'genres':
@@ -45,4 +41,4 @@ def answer_query(user_prompt: str) -> str:
         raise Exception(f'Could not ascertain user intent: {topic} -> {data}')
     movies = query_database(statement)
     prompt = f'Given these movies: {movies} answer this question: {user_prompt}'
-    return call_llm(prompt=prompt)
+    return call_llm(prompt=prompt, model_type=model_type, retry=retry)
